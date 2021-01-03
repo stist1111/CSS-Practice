2295784150a1b334217f5bc809bb2072fecf434e
Tae In Song <stist1111@naver.com>
2021-01-03 15:19:14 +0900

    섹션 16 (Transitions & Animations)

--- shared.js
+++ shared.js
@@ -4,13 +4,19 @@ let modalNoButton = document.querySelector('.modal__action--negative');
 let selectPlanButtons = document.querySelectorAll('.plan button');
 let toggleButton = document.querySelector('.toggle-button');
 let mobileNav = document.querySelector('.mobile-nav');
+let ctaButton = document.querySelector('.main-nav__item--cta');
 
 for(let i=0 ; i < selectPlanButtons.length ; i++){
     selectPlanButtons[i].addEventListener('click', function(){
         // modal.style.display = 'block';
-        // backdrop.style.display = 'block';
+       
         modal.classList.add('open');
-        backdrop.classList.add('open');
+       
+        backdrop.style.display = 'block';
+
+        setTimeout(function() {
+            backdrop.classList.add('open');
+        }, 10);
     });
 }
 
@@ -25,19 +31,40 @@ if(modalNoButton){
 }
 
 function closeModal() {
-    // backdrop.style.display = 'none';
     // modal.style.display = 'none';
+  
     if(modal){
         modal.classList.remove('open');
     }
+
     backdrop.classList.remove('open');
+
+    setTimeout(function() {
+        backdrop.style.display = 'none';
+    }, 10);
 }
 
 toggleButton.addEventListener('click', function(){
     // mobileNav.style.display = 'block';
-    // backdrop.style.display = 'block';
+    
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    
+    backdrop.style.display = 'block';
+
+    setTimeout(function() {
+        backdrop.classList.add('open');
+    }, 10);
+   
 });
 
-console.log(toggleButton);
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('Animation started',event);
+})
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('Animation endded',event);
+})
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log('Animation iteration',event);
+})
